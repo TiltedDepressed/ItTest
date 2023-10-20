ffc0115006b8ee9b33a277d871588b0cdc098100
rotoyouoi <rotoyouoi@yandex.ru>
2023-10-20 09:07:43 +0500

    initial commit

--- app/src/main/java/com/example/ittestapplication/activities/AuthActivity.kt
+++ app/src/main/java/com/example/ittestapplication/activities/AuthActivity.kt
@@ -1,11 +1,12 @@
-package com.example.ittestapplication
+package com.example.ittestapplication.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ittestapplication.R
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_launch)
     }
 }
